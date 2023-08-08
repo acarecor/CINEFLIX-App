c206d7972bbed64bf962ae5af233bde7b9ffcf8b
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-08-08 09:29:11 +0200

    added  bootstrap components to movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,22 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ( { movie, onMovieClick }) => {
     return (
-        <div
-        onClick= { ()=> {
-            onMovieClick(movie);
-        }}
-        >
-            {movie.title}
-        </div>
+        <Card className="h-100">
+            <Card.Img variant="top" src={movie.imagePath} />
+            <Card.Body>
+                <Card.Title> {movie.title}</Card.Title>
+                <Card.Text>{movie.description}</Card.Text>
+                <Card.Text>{movie.year}</Card.Text>
+                <Button onClick= { ()=> 
+                 onMovieClick(movie)} variant="link">Open
+                 </Button>
+       
+            </Card.Body>
+
+        </Card>
+       
     );
 };
 
